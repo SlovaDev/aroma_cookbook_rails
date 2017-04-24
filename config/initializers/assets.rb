146533af12a1cb09585e1667ae98e77b4abb2d09
Grant <gcg4wg@virginia.edu>
2017-04-23 22:54:18 -0400

    Bring in template assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( modernizr-2.8.3.min.js phone.css dashboard.css phone.js )
+Rails.application.config.assets.precompile += %w( fixedsticky.js master.js new-age.js phone.js )
+Rails.application.config.assets.precompile += %w( cusine.css.scss custom.css.scss mast.scss phone.scc.scss )
+Rails.application.config.assets.paths << Rails.root.join("app", "assets")
