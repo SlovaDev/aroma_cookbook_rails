49a09a8bfa0492883d28590adbe4b8c349b0cf1d
Grant Garland <ggarland@salesforce.com>
2017-05-04 15:34:19 -0400

    Mock out cuisines

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale
   get '/contact', to: 'help#contact', as: :contact
+  get '/help', to: 'help#index'
+  
   devise_for :users
-
-  resources :help, only: [:index, :show]
 
   resources :cuisines, only: [:index, :show]
   resources :recipes, except: [:index]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,40 @@
 # Set up default cuisines
 include Generator
 default_cuisines = Cuisine.create([
-  { name: "Appetizer" },
-  { name: "Bread" },
-  { name: "Breakfast" },
-  { name: "Soup" },
-  { name: "Entree" },
-  { name: "Salad" },
-  { name: "Dessert" },
-  { name: "Drink" },
-  { name: "Miscellaneous" },
+  { 
+  	name: "Appetizer",
+  	image_url: "appetizer.svg"
+  },
+  { 
+  	name: "Entree",
+  	image_url: "entree.svg"
+  },
+  { 
+  	name: "Dessert",
+  	image_url: "dessert.svg"
+  },
+  {
+  	name: "Bread",
+  	image_url: "bread.svg"
+  },
+  { 
+  	name: "Soup",
+  	image_url: "soup.svg"
+  },
+  { 
+  	name: "Salad",
+  	image_url: "salad.svg"
+  },
+  { 
+  	name: "Breakfast",
+  	image_url: "breakfast.svg"
+  },
+  { 
+  	name: "Drink",
+  	image_url: "drink.svg"
+  },
+  { 
+  	name: "Miscellaneous",
+  	image_url: "misc.svg"
+  },
 ])
