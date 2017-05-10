f7308e230010c5613c032fa68fdef9e4d1013168
Grant <gcg4wg@virginia.edu>
2017-05-09 22:34:25 -0400

    finish recipe new layout

--- app/helpers/cuisines_helper.rb
+++ app/helpers/cuisines_helper.rb
@@ -1,5 +1,2 @@
 module CuisinesHelper
-	def cuisine_categories
-		I18n.t(:cuisines).map { |key, value| [ value, key ] }
-	end
 end
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,5 @@
 module RecipesHelper
+  def cuisine_categories
+    I18n.t(:cuisines).collect { |key, value| [ value, key ] }
+  end
 end
